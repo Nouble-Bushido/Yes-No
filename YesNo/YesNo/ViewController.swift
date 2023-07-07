14da2d319b4eb9d6717fc39e706c30855a1722a0
Artem <artchzhen@gmail.com>
2023-07-07 11:30:58 +0900

    added project Yes/No, added 3 labels, 1 button, 1 imageView

---
+++ YesNo/YesNo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YesNo
+//
+//  Created by Артем Чжен on 06/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var yesLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var noLabel: UILabel!
+    @IBOutlet weak var getAnswerButton: UIButton!
+    @IBOutlet weak var noAnswerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+    
+    @IBAction func pressButton(_ sender: Any) {
+    }
+
+}
