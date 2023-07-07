47059c3b19ec01949a2973d157235f109d85f702
Artem <artchzhen@gmail.com>
2023-07-07 11:40:50 +0900

    added switch answer, and call method hideLabels

--- YesNo/YesNo/ViewController.swift
+++ YesNo/YesNo/ViewController.swift
@@ -17,39 +17,50 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    getAnswerButton.layer.cornerRadius = 8
-                labelHidden()
-    
+        getAnswerButton.layer.cornerRadius = 8
+        labelHidden()
+        
     }
     
     @IBAction func pressButton(_ sender: Any) {
         let numbers = [1, 2, 3]
-                let randomIndex = Int.random(in: 0..<numbers.count)
-                let answer = numbers[randomIndex]
+        let randomIndex = Int.random(in: 0..<numbers.count)
+        let answer = numbers[randomIndex]
+        
+        self.hideLabels {
+            switch answer {
+            case 1:
+                self.yesLabel.isHidden = false
+            case 2:
+                self.noLabel.isHidden = false
+            default:
+                self.noAnswerLabel.isHidden = false
+            }
+        }
     }
     
     func hideLabels(completion: @escaping () -> Void) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.yesLabel.alpha = 0
+            self.noLabel.alpha = 0
+            self.noAnswerLabel.alpha = 0
+            self.getAnswerButton.tintColor = .blue
+        }, completion: { _ in
+            self.labelHidden()
             UIView.animate(withDuration: 0.3, animations: {
-                self.yesLabel.alpha = 0
-                self.noLabel.alpha = 0
-                self.noAnswerLabel.alpha = 0
-                self.getAnswerButton.tintColor = .blue
+                self.yesLabel.alpha = 1
+                self.noLabel.alpha = 1
+                self.noAnswerLabel.alpha = 1
+                self.getAnswerButton.tintColor = .white
             }, completion: { _ in
-                self.labelHidden()
-                UIView.animate(withDuration: 0.3, animations: {
-                    self.yesLabel.alpha = 1
-                    self.noLabel.alpha = 1
-                    self.noAnswerLabel.alpha = 1
-                    self.getAnswerButton.tintColor = .white
-                }, completion: { _ in
-                    completion()
-                })
+                completion()
             })
-        }
+        })
+    }
     
     func labelHidden() {
-           yesLabel.isHidden = true
-           noLabel.isHidden = true
-           noAnswerLabel.isHidden = true
-       }
+        yesLabel.isHidden = true
+        noLabel.isHidden = true
+        noAnswerLabel.isHidden = true
+    }
 }
