19424ad1bae7e40889bc24f04632377b08d54128
Artem <artchzhen@gmail.com>
2023-07-07 11:35:13 +0900

    added cornerRadius to button and used method labelHidden in viewDidLoad

--- YesNo/YesNo/ViewController.swift
+++ YesNo/YesNo/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    getAnswerButton.layer.cornerRadius = 8
+                labelHidden()
     
     }
     
